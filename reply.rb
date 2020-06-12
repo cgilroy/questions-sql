690ca9ae90ede7c9a615815c3c329784f76ca76e
CJ Gilroy <c.gilroy9@gmail.com>
2020-06-12 14:02:24 -0600

    Add ::find_by_question_id to Reply class

--- reply.rb
+++ reply.rb
@@ -14,6 +14,19 @@ class Reply
         reply_data.map { |data| Reply.new(data) }
     end
 
+    def self.find_by_question_id(question_id)
+        reply_data = QuestionsDatabase.execute(<<-SQL, question_id)
+            SELECT
+                *
+            FROM
+                replies
+            WHERE
+                replies.question_id = ?
+        SQL
+        return nil if reply_data.nil?
+        reply_data.map { |data| Reply.new(data) }
+    end
+
     def initialize(options)
         @id, @question_id, @parent_id, @body, @user_id = options.values_at('id','question_id','parent_id','body','user_id')
     end
