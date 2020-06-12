bec58f4b9afffe9d804d715192742a61293d640c
CJ Gilroy <c.gilroy9@gmail.com>
2020-06-12 13:42:49 -0600

    Add Reply class

---
+++ reply.rb
@@ -0,0 +1,20 @@
+require_relative 'questions_database'
+
+class Reply
+    def self.find_by_user_id(user_id)
+        reply_data = QuestionsDatabase.execute(<<-SQL, user_id)
+            SELECT
+                *
+            FROM
+                replies
+            WHERE
+                replies.user_id = ?
+        SQL
+        return nil if reply_data.nil?
+        reply_data.map { |data| Reply.new(data) }
+    end
+
+    def initialize(options)
+        @id, @question_id, @parent_id, @body, @user_id = options.values_at('id','question_id','parent_id','body','user_id')
+    end
+end
